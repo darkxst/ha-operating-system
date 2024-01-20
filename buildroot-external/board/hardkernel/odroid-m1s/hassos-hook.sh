43320e80551e87723886974dfd30c966054e86ee
Tim Lunn <tim@feathertop.org>
2024-01-20 15:25:06 +1100

    Install Vu8s overlay file

--- buildroot-external/board/hardkernel/odroid-m1s/hassos-hook.sh
+++ buildroot-external/board/hardkernel/odroid-m1s/hassos-hook.sh
@@ -8,6 +8,8 @@ function hassos_pre_image() {
     cp "${BINARIES_DIR}/boot.scr" "${BOOT_DATA}/boot.scr"
     cp "${BINARIES_DIR}"/*.dtb "${BOOT_DATA}/"
 
+    mkdir -p "${BOOT_DATA}/overlays"
+    cp "${BINARIES_DIR}"/*.dtbo "${BOOT_DATA}/overlays/"
     cp "${BOARD_DIR}/boot-env.txt" "${BOOT_DATA}/haos-config.txt"
     cp "${BOARD_DIR}/cmdline.txt" "${BOOT_DATA}/cmdline.txt"
 
